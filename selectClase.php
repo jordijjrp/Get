940516ff98f24fa784482fcf1c8de4502cccb990
Reyes <jordinp062@gmail.com>
2025-03-25 20:11:55 +0100

    Add files via upload

---
+++ selectClase.php
@@ -0,0 +1,45 @@
+<?php
+class Usuarios {
+    private $conexion;
+
+    public function __construct($conexion) {
+        $this->conexion = $conexion;
+    }
+
+    public function obtenerUsuarios() {
+        try {
+            $sql = "SELECT * FROM usuarios";
+            $resultado = $this->conexion->query($sql);
+            
+            if ($resultado->num_rows > 0) {
+                echo "<table border='1'>
+                        <tr>
+                            <th>ID</th>
+                            <th>Usuario</th>
+                            <th>Nombre</th>
+                            <th>Contraseña</th>
+                        </tr>";
+                
+                while ($fila = $resultado->fetch_assoc()) {
+                    echo "<tr>
+                            <td>{$fila['id']}</td>
+                            <td>{$fila['usuario']}</td>
+                            <td>{$fila['nombre']}</td>
+                            <td>{$fila['contraseña']}</td>
+                        </tr>";
+                }
+                
+                echo "</table>";
+            } else {
+                echo "No hay usuarios registrados.";
+            }
+        } catch (Exception $e) {
+            echo "<h1 style='color:#f00'>Error en la consulta: </h1>" . $e->getMessage();
+        }
+    }
+}
+
+include 'config.php';
+$usuarios = new Usuarios($conexion);
+$usuarios->obtenerUsuarios();
+?>
