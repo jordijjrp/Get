f3a8a1e7087e32bb7d4da1949d1e5952d912d1f9
Reyes <jordinp062@gmail.com>
2025-03-25 19:58:23 +0100

    Add files via upload

--- select.php
+++ select.php
@@ -1,31 +1,35 @@
 <?php
 include 'config.php';
 
-$sql = "SELECT id, usuario, nombre, contraseña FROM usuarios";
-$resultado = $conexion->query($sql);
+try {
+    $sql = "SELECT * FROM usuarios";
+    $resultado = $conexion->query($sql);
 
-if ($resultado->num_rows > 0) {
-    echo "<table border='1'>
-            <tr>
-                <th>ID</th>
-                <th>Usuario</th>
-                <th>Nombre</th>
-                <th>Contraseña</th>
-            </tr>";
-    
-            while ($fila = $resultado->fetch_assoc()) {
-                echo "<tr>
-                        <td>{$fila['id']}</td>
-                        <td>{$fila['usuario']}</td>
-                        <td>{$fila['nombre']}</td>
-                        <td>{$fila['contraseña']}</td>
-                    </tr>";
-            }
-    
-    echo "</table>";
-} else {
-    echo "No hay usuarios registrados.";
+    if ($resultado->num_rows > 0) {
+        echo "<table border='1'>
+                <tr>
+                    <th>ID</th>
+                    <th>Usuario</th>
+                    <th>Nombre</th>
+                    <th>Contraseña</th>
+                </tr>";
+
+        while ($fila = $resultado->fetch_assoc()) {
+            echo "<tr>
+                    <td>{$fila['id']}</td>
+                    <td>{$fila['usuario']}</td>
+                    <td>{$fila['nombre']}</td>
+                    <td>{$fila['contraseña']}</td>
+                </tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "No hay usuarios registrados.";
+    }
+
+    $conexion->close();
+} catch (Exception $e) {
+    echo "<h1 style='color:#f00'>Error en la consulta: </h1>" . $e->getMessage();
 }
-
-$conexion->close();
 ?>
